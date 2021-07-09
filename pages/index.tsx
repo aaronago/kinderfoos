b826cb4c6ee77eb0a82394a6526232ab5a7c3e37
Aaron Gottlieb <atticus19@gmail.com>
2021-07-09 08:23:55 -0600

    home title tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,12 @@ import { SkipNavContent } from '@reach/skip-nav'
 
 import Page from '@components/page'
 import ConfContent from '@components/index'
-import { META_DESCRIPTION } from '@lib/constants'
 
 export default function Conf() {
   const { query } = useRouter()
   const meta = {
-    title: 'Demo - Virtual Event Starter Kit',
-    description: META_DESCRIPTION,
+    title: 'Kinder Foos',
+    description: 'Kinder feet kinder world',
   }
   const ticketNumber = query.ticketNumber?.toString()
   const defaultUserData = {
